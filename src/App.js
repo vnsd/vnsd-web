7f812d61ffaa65144561c5eb8e58a4e943fbbe7b
ngoc tho nguyen <ngocthonguyen@MacBook-Pro-de-nguyen.local>
2019-08-06 21:37:12 +0200

    Ok

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             <Router>
                 <div className="container">
                     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                        <Link to={'/'} className="navbar-brand">Vietnam Startups Directory</Link>
+                        <Link to={'/'} className="navbar-brand">Vietnam Startup Directory</Link>
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul className="navbar-nav mr-auto">
                                 <li className="nav-item">
